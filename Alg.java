ee89427b66f53359966e47c9a98d85929e72d6d7
Alexander Ge <gealexander.11@gmail.com>
2011-06-18 15:10:13 -0400

    matingseason works?

--- Alg.java
+++ Alg.java
@@ -2,26 +2,27 @@ import java.util.*;
 
 public class Alg {
 
-	/*Alg as a process*/
+	/**Alg as a process*/
 	private final int NUMIND_POP_SIZE = 30;//although it is a valid variable, pop size 
 						//cannot vary between algs within
 						//a masteralg because the algs cannot be adequately
 						//compared without identical populations
 	private long numIndTotalFitness;
-	private int[] sorted; //indices of NumInds in pop. in order of fitness
+	private int[] sorted; //indices of NumInds in pop in order of fitness
 				//sorted[0] = best
-				//name generic things  like "fitness"
+				
 	private long numIndAvgFitness;	//according to the class
 	private NumInd[] numIndPop;	//to avoid confusion
 	private double changeInAvgNumIndFitness;
-	/* *******/
+	private long changeInTotNumIndFitness;
+	/*********/
 	
 	
-	/*Alg as an individual*/
+	/**Alg as an individual*/
 	private long algFitness; 
 	private final int ALG_CHROMOSOME_SIZE = 6;
 	private final int ALG_TRUE_RATE = 2;
-	/* ******/
+	/********/
 	
 	
 	
@@ -77,46 +78,40 @@ public class Alg {
 	of steps in the process. add any missing, very important ones*/
 
 	public Alg() {
-		/*Alg as an individual*/
-			algChromosome = new Gene[ALG_CHROMOSOME_SIZE];
-			algChromosome[0] = new Gene(SELECTION_GENE_SIZE, ALG_TRUE_RATE);
-			algChromosome[1] = new Gene(ELITISM_GENE_SIZE, ALG_TRUE_RATE);
-			algChromosome[2] = new Gene(CROSSOVER_GENE_SIZE, ALG_TRUE_RATE);
-			algChromosome[3] = new Gene(NUMIND_MUTATE_RATE_GENE_SIZE, ALG_TRUE_RATE);
-			algChromosome[4] = new Gene(NUMIND_GENE_MUTATE_RATE_GENE_SIZE, ALG_TRUE_RATE);
-			algChromosome[5] = new Gene(NUMIND_GENE_BIT_MUTATE_RATE_GENE_SIZE, ALG_TRUE_RATE);
-		/* ***********/
+		
+		/**Set Genes in Alg's chromosome*/
+		algChromosome = new Gene[ALG_CHROMOSOME_SIZE];
+		algChromosome[0] = new Gene(SELECTION_GENE_SIZE, ALG_TRUE_RATE);
+		algChromosome[1] = new Gene(ELITISM_GENE_SIZE, ALG_TRUE_RATE);
+		algChromosome[2] = new Gene(CROSSOVER_GENE_SIZE, ALG_TRUE_RATE);
+		algChromosome[3] = new Gene(NUMIND_MUTATE_RATE_GENE_SIZE, ALG_TRUE_RATE);
+		algChromosome[4] = new Gene(NUMIND_GENE_MUTATE_RATE_GENE_SIZE, ALG_TRUE_RATE);
+		algChromosome[5] = new Gene(NUMIND_GENE_BIT_MUTATE_RATE_GENE_SIZE, ALG_TRUE_RATE);
+		/********************************/
 		
 		for(Gene g: algChromosome){
 			g.setValue();
 		}
-		/*set instance variables to gene values. correct them if they are outside natural limits*/
+		
+		/**set instance variables to gene values. Scale them so they are not outside natural limits*/
+		
 		selection = (int) algChromosome[0].getValue();
 		if(selection < 2){
 			selection = 2;
 		}
-		//instead of assigning all out-of-range numbers to the min or max, we could
-		//scale the range of possibly values down to the range we want
-		//selection = (int) ((double)getValue() / getMaxValue() * (NUMIND_POP_SIZE-2)) + 2;
-		if(selection > NUMIND_POP_SIZE){
-			selection = NUMIND_POP_SIZE;
-		}
+		selection = (int) ((double) algChromosome[0].getValue() / algChromosome[0].getMaxValue() * (NUMIND_POP_SIZE-2)) + 2;
 		
-		elitism = (int) algChromosome[1].getValue();
-		//elitism = (int) ((double) getValue() / getMaxValue() * NUMIND_POP_SIZE);
-		if(elitism > NUMIND_POP_SIZE){
-			elitism = NUMIND_POP_SIZE;
-		}
+		elitism = (int) ((double) algChromosome[1].getValue() / algChromosome[1].getMaxValue() * NUMIND_POP_SIZE);
 		
-		crossover = (int) algChromosome[2].getValue();
-		//crossover = (int) ((double) getValue() / getMaxValue() * NumInd.NUMIND_CHROMOSOME_SIZE);
-		if(crossover > NumInd.NUMIND_CHROMOSOME_SIZE){
-			crossover = NumInd.NUMIND_CHROMOSOME_SIZE;
-		}
+		crossover = (int) ((double) algChromosome[2].getValue() / algChromosome[2].getMaxValue() * NumInd.NUMIND_CHROMOSOME_SIZE);
 		
 		numIndMutateRate = (double) algChromosome[3].getValue() / 1023;
+		
 		numIndGeneMutateRate = (double) algChromosome[4].getValue() / 1023;
+		
 		numIndGeneBitMutateRate = (double) algChromosome[5].getValue() / 1023;
+		
+		/*******************************************/
 		
 		sorted = new int[NUMIND_POP_SIZE];
 		numIndPop = new NumInd[NUMIND_POP_SIZE];
@@ -124,8 +119,6 @@ public class Alg {
 			numIndPop[i] = new NumInd();
 			sorted[i] = i;
 		}
-		
-		
 	}
 
 	/**
@@ -157,14 +150,6 @@ public class Alg {
 		return numIndPop[index];
 	}
 
-	public void setNumIndTotalFitness(int[] subPop) {
-
-		numIndTotalFitness = 0;
-		for(int i = 0; i < subPop.length; i++){
-			numIndTotalFitness += numIndPop[subPop[i]].getNumIndFitness();
-		}
-	}
-
 	public NumInd[] copyNumIndPop() {
 
 		NumInd[] copy = new NumInd[NUMIND_POP_SIZE];
@@ -174,12 +159,46 @@ public class Alg {
 		return copy;
 	}
 
-	public void setNumIndAvgFitness(int[] popIndices) {
+	public void setNumIndTotAvgFitness() {
 
+		numIndTotalFitness = 0;
+		for(int i = 0; i < numIndPop.length; i++){
+			numIndTotalFitness += numIndPop[i].getNumIndFitness();
+		}
+		
 		numIndAvgFitness = 0;
-		setNumIndTotalFitness(popIndices);
-		numIndAvgFitness = numIndTotalFitness / NUMIND_POP_SIZE;
+		numIndAvgFitness =  numIndTotalFitness / NUMIND_POP_SIZE;
 	}
+	
+	public void setChangeInTotAvgNumIndFitness(NumInd[] oldPop) {
+		
+		long oldFit = 0;
+		for(int i = 0; i < oldPop.length; i++){
+			oldFit += oldPop[i].getNumIndFitness();
+		}
+		
+		long newFit = 0;
+		for(int i = 0; i < numIndPop.length; i++){
+			newFit += this.numIndPop[i].getNumIndFitness();
+		}
+		
+		changeInTotNumIndFitness = newFit - oldFit;
+		
+		double totalChange = newFit - oldFit;
+		
+		changeInAvgNumIndFitness =  totalChange / ((double) NUMIND_POP_SIZE);
+	}
+	
+	public long setSubPopTotalFitness(int[] subPop) {
+		
+		long end = 0;
+		for (int i = 0; i < subPop.length; i++) {
+			end += numIndPop[subPop[i]].getNumIndFitness();
+		}
+		return end;
+	}
+		
+		
 
 	//sorts the top n NumInds in population according to fitness value
 	public void sort(int n) {
@@ -204,6 +223,9 @@ public class Alg {
 
 	public void matingSeason() {
 
+		setNumIndTotAvgFitness();
+		double oldAvg = numIndAvgFitness;
+		long oldTot = numIndTotalFitness;
 		NumInd[] oldPop = numIndPop;
 		NumInd[] temPop = new NumInd[NUMIND_POP_SIZE];
 
@@ -217,10 +239,10 @@ public class Alg {
 
 		//fill remaining population with offspring
 		for(int m = elitism; m < NUMIND_POP_SIZE; m++) {
-			int mother = selectParent(-1); //-1 doesnt exist but it has to take an int
+			int mother = selectParent(-1);
 			int father = selectParent(mother); //select a father who is not the mother 
 							
-			temPop[m] = mate(mother, father);//yea?
+			temPop[m] = mate(mother, father);
 		}
 		numIndPop = temPop;
 
@@ -229,8 +251,14 @@ public class Alg {
 		
 		/** list of new individuals (with fitnesses), mean / median fitness, std dev fitness, 
 		total change in fitness, mean change in fitness*/
-		setChangeInAvgNumIndFitness(oldPop);
-		
+		setNumIndTotAvgFitness();
+		setChangeInTotAvgNumIndFitness(oldPop);
+		System.out.println("oldpop average fitness: " + oldAvg);
+		System.out.println("newpop average fitness: " + numIndAvgFitness);
+		System.out.println("change in average fitness: " + changeInAvgNumIndFitness);
+		System.out.println("oldpop total fitness: " + oldTot);
+		System.out.println("newpop total fitness: " + numIndTotalFitness);
+		System.out.println("change in total fitness: " + changeInTotNumIndFitness);
 		
 		
 	}
@@ -287,11 +315,11 @@ public class Alg {
 		//mating, and does not contain the same NumInd twice
 		return roulette(subPop);
 	}
-
+	
 	//roulette selection with array of numIndPop indices
 	public int roulette(int[] pop) {
 		
-		long ball = (long) (Math.random() * numIndTotalFitness);
+		long ball = (long) (Math.random() * setSubPopTotalFitness(pop));
 		long sum = 0;
 		for(int i = 0; i < pop.length; i++){
 			sum += numIndPop[pop[i]].getNumIndFitness();
@@ -302,23 +330,8 @@ public class Alg {
 		return -1;
 	}
 	
-	public void setChangeInAvgNumIndFitness(NumInd[] oldPop) {
-		
-		long oldFit = 0;
-		for(int i = 0; i < oldPop.length; i++){
-			oldFit += oldPop[i].getNumIndFitness();
-		}
-		
-		long newFit = 0;
-		for(int i = 0; i < numIndPop.length; i++){
-			newFit += this.numIndPop[i].getNumIndFitness();
-		}
-		
-		double totalChange = newFit - oldFit;
-		
-		changeInAvgNumIndFitness =  totalChange / NUMIND_POP_SIZE;
-	}
-
+	
+	
 	//stuff for Alg as an individual	
 	/*
 	public void setAlgFitness(){
@@ -330,15 +343,18 @@ public class Alg {
 
 	public static void main(String[] args) {                         
 
-	/*	Alg a = new Alg();
+		/*	
+		Alg a = new Alg();
 		System.out.println(a);
-		a.setNumIndAvgFitness();
+		a.setNumIndTotAvgFitness();
 		System.out.println(a.numIndAvgFitness + " " + NumInd.NUMIND_MAX_FITNESS);
 		System.out.println((double)NumInd.NUMIND_MAX_FITNESS / a.numIndAvgFitness);
-		System.out.println(a.numIndPop[0]); */
+		System.out.println(a.numIndPop[0]); 
+		*/
 
 
-		/*int[] momAndDads = new int[3];
+		/*
+		int[] momAndDads = new int[3];
 		momAndDads[0] = 0;
 		momAndDads[1] = 1;
 		momAndDads[2] = 2;
@@ -354,14 +370,18 @@ public class Alg {
 		}
 		*/
 
-		/*Alg a = new Alg();
+		/*
+		Alg a = new Alg();
 		System.out.println("Alg: \n" + a + "\n");
 
 		System.out.println(a.numIndTotalFitness);
 
 		System.out.println(a.numIndAvgFitness);
 
-		System.out.println(a.sorted);*/
+		System.out.println(a.sorted);
+		*/
+		
+		/*
 		Alg a = new Alg();
 		System.out.println(a); 
 
@@ -374,8 +394,14 @@ public class Alg {
 		System.out.println("parent2: " + a.numIndPop[x]);
 		
 		System.out.println(a.mate(4,x));
+		*/
 		
-		
+		Alg a = new Alg();
+		System.out.println(a);
+		a.setNumIndTotAvgFitness();
+		System.out.println("Average Fitness: " + a.numIndAvgFitness);
+		a.matingSeason();
+		System.out.println(a);
 		
 	}
 
